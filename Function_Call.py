b7788aff1c230f5981e3c1bd3463cc10dedf8721
cgnannan <84908809@qq.com>
2023-06-25 19:57:28 +0800

    Initializing a private Function_Call Study repository

---
+++ Function_Call.py
@@ -0,0 +1,129 @@
+import os
+import openai
+import json
+openai.api_key=os.getenv("OPENAI_API_KEY")
+
+
+#Example dummy function hard coded to return the same weather
+#In production ,this could be your backend API or an external API
+def get_current_weather(location,unit):
+    """Get the current weather in a given location"""
+    weather_info={
+        "location":location,
+        "temperature":"22",
+        "unit":unit,
+    }
+    return json.dumps(weather_info)
+
+def get_n_weather_forecast(location,unit,num_days):
+    """Get an N-day weather forecast"""
+    forecast_info={
+        "location":location,
+        "temperature":"30",
+        "unit":unit,
+        "num_days": num_days,
+        "forecast":["rainy"],
+    }
+    return json.dumps(forecast_info)
+
+def run_conversation():
+    #Step1:send the conversation and available functions to GPT
+    messages = [
+        {"role": "system", "content": "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."},
+        {"role": "user", "content": "What's the weather like in Beijing?"}
+        ]
+
+    functions = [
+        {
+            "name": "get_current_weather",
+            "description": "Get the current weather in a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA",
+                    },
+                    "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                },
+                "required": ["location","unit"],
+            },
+        },
+        {
+            "name": "get_n_weather_forecast",
+            "description": "Get an N-day weather forecast",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state,e.g. San Francisco, CA",
+                    },
+                    "unit": {
+                        "type": "string",
+                        "enum": ["celsius","fahrenheit"],
+                        "description": "The tempeture unit to use.Infer this from the users location."
+                    },
+                    "num_days": {
+                        "type": "integer",
+                        "description": "The number of days to forecast",
+                    }
+                },
+                "required": ["location","unit","num_days"]
+            }
+        }
+    ]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        messages=messages,
+        functions=functions,
+        function_call="auto",  # auto is default, but we'll be explicit
+    )
+    response_message = response["choices"][0]["message"]
+
+    #Step 2:check if GPT wanted to call a function
+    if response_message.get("function_call"):
+        # Step 3: call the function
+        # Note: the JSON response may not always be valid; be sure to handle errors
+        
+        available_functions = {
+            "get_current_weather": get_current_weather,
+            "get_n_weather_forecast": get_n_weather_forecast
+        }  # only one function in this example, but you can have multiple
+        
+        function_name = response_message["function_call"]["name"]
+        fuction_to_call = available_functions[function_name]
+
+        function_args = json.loads(response_message["function_call"]["arguments"])
+
+        if function_name == "get_current_weather":
+             function_response = fuction_to_call(
+                 location=function_args.get("location"),
+                 unit=function_args.get("unit")
+             )
+        elif function_name == "get_n_weather_forecast":
+             function_response = fuction_to_call(
+                 location=function_args.get("location"),
+                 unit=function_args.get("unit"),
+                 num_days=function_args.get("num_days")
+             )
+
+        # Step 4: send the info on the function call and function response to GPT
+        #messages.append(response_message)  # extend conversation with assistant's reply
+        messages.append(
+            {
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            }
+        )  # extend conversation with function response
+
+        second_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=messages,
+        )  # get a new response from GPT where it can see the function response
+        #return second_response['choices'][0]['message']['content']
+        user_facing_message=second_response["choices"][0]["message"]["content"]
+        return user_facing_message
+        
+print(run_conversation())
---
+++ Function_Call_CookBook.py
@@ -0,0 +1,206 @@
+import os
+import openai
+import json
+import requests
+import sqlite3
+from tenacity import retry,wait_random_exponential,stop_after_attempt
+from termcolor import colored
+openai.api_key=os.getenv("OPENAI_API_KEY")
+
+GPT_MODEL="gpt-3.5-turbo-0613"
+
+#First let's define a few utilities for making calls to the Chat Completions API and for maintaining and keeping track of the conversation state.
+@retry(wait=wait_random_exponential(min=1, max=40), stop=stop_after_attempt(3))
+def chat_completion_request(messages, functions=None, function_call=None, model=GPT_MODEL):
+    json_data = {"model": model, "messages": messages}
+    if functions is not None:
+        json_data.update({"functions": functions})
+    if function_call is not None:
+        json_data.update({"function_call": function_call})
+    try:
+        response = openai.ChatCompletion.create(**json_data)
+        return response
+    except Exception as e:
+        print("Unable to generate ChatCompletion response")
+        print(f"Exception: {e}")
+        return e
+
+def pretty_print_conversation(messages):
+    role_to_color = {
+        "system": "red",
+        "user": "green",
+        "assistant": "blue",
+        "function": "magenta",
+    }
+    formatted_messages = []
+    for message in messages:
+        if message["role"] == "system":
+            formatted_messages.append(f"system: {message['content']}\n")
+        elif message["role"] == "user":
+            formatted_messages.append(f"user: {message['content']}\n")
+        elif message["role"] == "assistant" and message.get("function_call"):
+            formatted_messages.append(f"assistant: {message['function_call']}\n")
+        elif message["role"] == "assistant" and not message.get("function_call"):
+            formatted_messages.append(f"assistant: {message['content']}\n")
+        elif message["role"] == "function":
+            formatted_messages.append(f"function ({message['name']}): {message['content']}\n")
+    for formatted_message in formatted_messages:
+        print(
+            colored(
+                formatted_message,
+                role_to_color[messages[formatted_messages.index(formatted_message)]["role"]],
+            )
+        )
+
+#Let's create some function specifications to interface with a hypothetical weather API. We'll pass these function specification to the Chat Completions API in order to generate function arguments that adhere to the specification.
+functions = [
+    {
+        "name": "get_current_weather",
+        "description": "Get the current weather",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA",
+                },
+                "format": {
+                    "type": "string",
+                    "enum": ["celsius", "fahrenheit"],
+                    "description": "The temperature unit to use. Infer this from the users location.",
+                },
+            },
+            "required": ["location", "format"],
+        },
+    },
+    {
+        "name": "get_n_day_weather_forecast",
+        "description": "Get an N-day weather forecast",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA",
+                },
+                "format": {
+                    "type": "string",
+                    "enum": ["celsius", "fahrenheit"],
+                    "description": "The temperature unit to use. Infer this from the users location.",
+                },
+                "num_days": {
+                    "type": "integer",
+                    "description": "The number of days to forecast",
+                }
+            },
+            "required": ["location", "format", "num_days"]
+        },
+    },
+]
+messages = []
+messages.append({"role": "system", "content": "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."})
+messages.append({"role": "user", "content": "what is the weather  going to be like in Toronto, Canada over the next 5 days"})
+chat_response = chat_completion_request(
+    messages, functions=functions
+)
+assistant_message = chat_response["choices"][0]["message"]
+function_name = assistant_message["function_call"]["name"]
+function_arg= assistant_message["function_call"]["arguments"]
+messages.append(assistant_message)
+
+print(assistant_message)
+
+#Specifying a function to execute SQL queries
+conn = sqlite3.connect("/Volumes/work/Project/AIGC/OpenAI/Function_Call/chinook.db")
+print("Opened database successfully")
+
+#define some helpful utility functions to extract data from a SQLite database.
+def get_table_names(conn):
+    """Return a list of table names."""
+    table_names = []
+    tables = conn.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    for table in tables.fetchall():
+        table_names.append(table[0])
+    return table_names
+
+
+def get_column_names(conn, table_name):
+    """Return a list of column names."""
+    column_names = []
+    columns = conn.execute(f"PRAGMA table_info('{table_name}');").fetchall()
+    for col in columns:
+        column_names.append(col[1])
+    return column_names
+
+
+def get_database_info(conn):
+    """Return a list of dicts containing the table name and columns for each table in the database."""
+    table_dicts = []
+    for table_name in get_table_names(conn):
+        columns_names = get_column_names(conn, table_name)
+        table_dicts.append({"table_name": table_name, "column_names": columns_names})
+    return table_dicts
+
+#use these utility functions to extract a representation of the database schema.
+database_schema_dict = get_database_info(conn)
+database_schema_string = "\n".join(
+    [
+        f"Table: {table['table_name']}\nColumns: {', '.join(table['column_names'])}"
+        for table in database_schema_dict
+    ]
+)
+
+functions = [
+    {
+        "name": "ask_database",
+        "description": "Use this function to answer user questions about music. Output should be a fully formed SQL query.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "query": {
+                    "type": "string",
+                    "description": f"""
+                            SQL query extracting info to answer the user's question.
+                            SQL should be written using this database schema:
+                            {database_schema_string}
+                            The query should be returned in plain text, not in JSON.
+                            """,
+                }
+            },
+            "required": ["query"],
+        },
+    }
+]
+
+#implement the function that will actually excute queries against the database.
+def ask_database(conn, query):
+    """Function to query SQLite database with a provided SQL query."""
+    try:
+        results = str(conn.execute(query).fetchall())
+    except Exception as e:
+        results = f"query failed with error: {e}"
+    return results
+
+def execute_function_call(message):
+    if message["function_call"]["name"] == "ask_database":
+        query = json.loads(message["function_call"]["arguments"])["query"]
+        results = ask_database(conn, query)
+    else:
+        results = f"Error: function {message['function_call']['name']} does not exist"
+    return results
+
+messages = []
+messages.append({"role": "system", "content": "Answer user questions by generating SQL queries against the Chinook Music Database."})
+messages.append({"role": "user", "content": "Hi, who are the top 5 artists by number of tracks and "})
+chat_response = chat_completion_request(messages, functions)
+assistant_message = chat_response["choices"][0]["message"]
+
+#extend conversation with function response for the second request
+if assistant_message.get("function_call"):
+    results = execute_function_call(assistant_message)
+    messages.append({"role": "function", "name": assistant_message["function_call"]["name"], "content": results})
+
+second_response = chat_completion_request(messages)    #get a new response from GPT where it can see system message,user message and function response
+
+pretty_print_conversation(messages)
+print(second_response["choices"][0]["message"]["content"])
