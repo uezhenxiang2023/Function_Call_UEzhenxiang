4b1c5ba949cefe86243aab1b206d5a58205dfe8f
cgnannan <84908809@qq.com>
2023-06-27 16:42:07 +0800

    Optimize fine-tuning function call logic

--- Function_Call.py
+++ Function_Call.py
@@ -1,24 +1,46 @@
-import os
-import openai
-import json
+# imports
 import ast  # for converting embeddings saved as strings back to arrays
+import openai  # for calling the OpenAI API
 import pandas as pd  # for storing text and embeddings data
 import tiktoken  # for counting tokens
 from scipy import spatial  # for calculating vector similarities for search
-
+import os
+import json
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # models
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_MODEL = "gpt-3.5-turbo-0613"
 
+df = None  # DataFrame initialization
+
 # download pre-chunked text and pre-computed embeddings
 # this file is ~200 MB, so may take a minute depending on your connection speed
 embeddings_path = "/Volumes/work/Project/AIGC/OpenAI/Function_Call/data/FIFA_World_Cup_2022.csv"
 
-df = None  # DataFrame initialization
+# Example dummy function hard coded to return the same weather
+# In production, this could be your backend API or an external API
+def get_current_weather(location, unit):
+    """Get the current weather in a given location"""
+    weather_info = {
+        "location": location,
+        "temperature": "22",
+        "unit": unit,
+    }
+    return json.dumps(weather_info)
 
-# search function
+
+def get_n_weather_forecast(location, unit, num_days):
+    """Get an N-day weather forecast"""
+    forecast_info = {
+        "location": location,
+        "temperature": "30",
+        "unit": unit,
+        "num_days": num_days,
+        "forecast": ["rainy"],
+    }
+    return json.dumps(forecast_info)
+
 def strings_ranked_by_relatedness(
     query: str,
     df: pd.DataFrame,
@@ -39,10 +61,11 @@ def strings_ranked_by_relatedness(
     strings, relatednesses = zip(*strings_and_relatednesses)
     return strings[:top_n], relatednesses[:top_n]
 
+
 def num_tokens(text: str, model: str = GPT_MODEL) -> int:
     """Return the number of tokens in a string."""
     encoding = tiktoken.encoding_for_model(model)
-    return len(encoding.encode(text)) 
+    return len(encoding.encode(text))
 
 def ask(
     query: str,
@@ -74,8 +97,7 @@ def ask(
         temperature=0
     )
     response_message = response["choices"][0]["message"]["content"]
-    return response_message
-
+    return response_message 
 
 def query_message(
     query: str,
@@ -99,38 +121,12 @@ def query_message(
             message += next_article
     return message + question
 
-
-# Example dummy function hard coded to return the same weather
-# In production, this could be your backend API or an external API
-def get_current_weather(location, unit):
-    """Get the current weather in a given location"""
-    weather_info = {
-        "location": location,
-        "temperature": "22",
-        "unit": unit,
-    }
-    return json.dumps(weather_info)
-
-
-def get_n_weather_forecast(location, unit, num_days):
-    """Get an N-day weather forecast"""
-    forecast_info = {
-        "location": location,
-        "temperature": "30",
-        "unit": unit,
-        "num_days": num_days,
-        "forecast": ["rainy"],
-    }
-    return json.dumps(forecast_info)
-
-
 def run_conversation():
     # Step1: send the conversation and available functions to GPT
     messages = [
-        {"role": "system", "content": "回答问题时，请跟客户提问所用的语言保持一致。比如用户用中文提问，你也用中文回答。"},
-        {"role": "user", "content": "2022年卡塔尔世界杯决里点球大战的最终比分是多少？最后冠军是谁"}
-        ]
-
+        {"role": "system", "content": "你是非常聪明的人工智能助手，旨在帮助用户解答问题。回答问题时，请跟客户提问所用的语言保持一致。比如用户用中文提问，你也用中文回答。"},
+        {"role": "user", "content": "What is the Qatar World Cup final score??"}
+    ]
     functions = [
         {
             "name": "ask",
@@ -183,7 +179,7 @@ def run_conversation():
                 },
                 "required": ["location", "unit", "num_days"]
             }
-        }
+        }   
     ]
     response = openai.ChatCompletion.create(
         model=GPT_MODEL,
@@ -192,42 +188,49 @@ def run_conversation():
         function_call="auto",  # auto is default, but we'll be explicit
     )
     response_message = response["choices"][0]["message"]
-    
+
     # Step 2: check if GPT wanted to call a function
     if response_message.get("function_call"):
-        # Step 3: call the function and pass the relevant arguments
+        # Step 3: call the function
+        # Note: the JSON response may not always be valid; be sure to handle errors
+        available_functions = {
+            "ask": ask,
+            "get_current_weather": get_current_weather,
+            "get_n_weather_forecast": get_n_weather_forecast
+        }  # three functions in this example
         function_name = response_message["function_call"]["name"]
+        fuction_to_call = available_functions[function_name]
         function_args = json.loads(response_message["function_call"]["arguments"])
-        if function_name == "ask":
-            query = function_args["query"]
-            user_facing_message = f"{ask(query)}"  # Call the modified 'ask' function
-        else:
-            if function_name == "get_current_weather":
-                location = function_args["location"]
-                unit = function_args["unit"]
-                function_response = get_current_weather(location, unit)
-            elif function_name == "get_n_weather_forecast":
-                location = function_args["location"]
-                unit = function_args["unit"]
-                num_days = function_args["num_days"]
-                function_response = get_n_weather_forecast(location, unit, num_days)
-            
-            messages.append(
-                {
-                    "role": "function",
-                    "name": function_name,
-                    "content": function_response,
-                }
-            )  # extend conversation with function response
+        if function_name =="ask":
+           function_response = fuction_to_call(query=function_args.get("query"))
+        elif  function_name =="get_current_weather":
+              function_response = fuction_to_call(
+                location=function_args.get("location"),
+                unit=function_args.get("unit")
+              )
+        elif  function_name =="get_n_weather_forecast":
+              function_response = fuction_to_call(
+                location=function_args.get("location"),
+                unit=function_args.get("unit"),
+                num_days=function_args.get("num_days"),
+              )
 
-            second_response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo-0613",
-                messages=messages,
-            )  # get a new response from GPT where it can see the function response
-            user_facing_message = second_response["choices"][0]["message"]["content"]
+        # Step 4: send the info on the function call and function response to GPT
+        messages.append(response_message)  # extend conversation with assistant's reply
+        messages.append(
+            {
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            }
+        )  # extend conversation with function response
+        second_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=messages,
+        )  # get a new response from GPT where it can see the function response
     else:
-        user_facing_message = response_message["content"]
+        second_response=response
 
-    return user_facing_message
+    return second_response["choices"][0]["message"]["content"]
 
 print(run_conversation())
