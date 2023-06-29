aac059d95cceb16f3c8f444faa5a6ee079b5d4c0
cgnannan <84908809@qq.com>
2023-06-29 19:09:52 +0800

    adjust the return method of run_function_calling()

--- Function_Call.py
+++ Function_Call.py
@@ -1,133 +1,103 @@
-# imports
-import ast  # for converting embeddings saved as strings back to arrays
-import openai  # for calling the OpenAI API
-import pandas as pd  # for storing text and embeddings data
-import tiktoken  # for counting tokens
-from scipy import spatial  # for calculating vector similarities for search
+import ast
+import openai
+import pandas as pd
+import tiktoken
+from scipy import spatial
 import os
 import json
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# models
-EMBEDDING_MODEL = "text-embedding-ada-002"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 GPT_MODEL = "gpt-3.5-turbo-0613"
 
-df = None  # DataFrame initialization
+class FunctionRunner:
+    def __init__(self, api_key, model, embeddings_path):
+        openai.api_key = api_key
+        self.EMBEDDING_MODEL = "text-embedding-ada-002"
+        self.GPT_MODEL = model
+        self.df = None
+        self.embeddings_path = embeddings_path
 
-# download pre-chunked text and pre-computed embeddings
-# this file is ~200 MB, so may take a minute depending on your connection speed
-embeddings_path = "/Volumes/work/Project/AIGC/OpenAI/Function_Call/data/FIFA_World_Cup_2022.csv"
+    def get_current_weather(self, location, unit):
+        weather_info = {
+            "location": location,
+            "temperature": "22",
+            "unit": unit,
+        }
+        return json.dumps(weather_info)
 
-# Example dummy function hard coded to return the same weather
-# In production, this could be your backend API or an external API
-def get_current_weather(location, unit):
-    """Get the current weather in a given location"""
-    weather_info = {
-        "location": location,
-        "temperature": "22",
-        "unit": unit,
-    }
-    return json.dumps(weather_info)
+    def get_n_weather_forecast(self, location, unit, num_days):
+        forecast_info = {
+            "location": location,
+            "temperature": "30",
+            "unit": unit,
+            "num_days": num_days,
+            "forecast": ["rainy"],
+        }
+        return json.dumps(forecast_info)
 
+    def strings_ranked_by_relatedness(
+        self,
+        query: str,
+        df: pd.DataFrame,
+        relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
+        top_n: int = 100
+    ) -> tuple[list[str], list[float]]:
+        query_embedding_response = openai.Embedding.create(
+            model=self.EMBEDDING_MODEL,
+            input=query,
+        )
+        query_embedding = query_embedding_response["data"][0]["embedding"]
+        strings_and_relatednesses = [
+            (row["text"], relatedness_fn(query_embedding, row["embedding"]))
+            for i, row in df.iterrows()
+        ]
+        strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+        strings, relatednesses = zip(*strings_and_relatednesses)
+        return strings[:top_n], relatednesses[:top_n]
 
-def get_n_weather_forecast(location, unit, num_days):
-    """Get an N-day weather forecast"""
-    forecast_info = {
-        "location": location,
-        "temperature": "30",
-        "unit": unit,
-        "num_days": num_days,
-        "forecast": ["rainy"],
-    }
-    return json.dumps(forecast_info)
+    def num_tokens(self, text: str, model: str) -> int:
+        encoding = tiktoken.encoding_for_model(model)
+        return len(encoding.encode(text))
 
-def strings_ranked_by_relatedness(
-    query: str,
-    df: pd.DataFrame,
-    relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
-    top_n: int = 100
-) -> tuple[list[str], list[float]]:
-    """Returns a list of strings and relatednesses, sorted from most related to least."""
-    query_embedding_response = openai.Embedding.create(
-        model=EMBEDDING_MODEL,
-        input=query,
-    )
-    query_embedding = query_embedding_response["data"][0]["embedding"]
-    strings_and_relatednesses = [
-        (row["text"], relatedness_fn(query_embedding, row["embedding"]))
-        for i, row in df.iterrows()
-    ]
-    strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
-    strings, relatednesses = zip(*strings_and_relatednesses)
-    return strings[:top_n], relatednesses[:top_n]
-
-
-def num_tokens(text: str, model: str = GPT_MODEL) -> int:
-    """Return the number of tokens in a string."""
-    encoding = tiktoken.encoding_for_model(model)
-    return len(encoding.encode(text))
-
-def ask(
-    query: str,
-    csv_path: str = embeddings_path,
-    df_cache: pd.DataFrame = None,
-    model: str = GPT_MODEL,
-    token_budget: int = 4096 - 500,
-    print_message: bool = False,
-) -> str:
-    """Answers a query using GPT and a dataframe of relevant texts and embeddings."""
-    global df  # reference the global DataFrame variable
-
-    if df_cache is None:
-        # Load the DataFrame from the CSV file
-        df = pd.read_csv(csv_path)
-        # Convert embeddings from CSV str type back to list type
-        df['embedding'] = df['embedding'].apply(ast.literal_eval)
-    else:
-        # Use the cached DataFrame
-        df = df_cache
-
-    message = query_message(query, df, model=model, token_budget=token_budget)
-    if print_message:
-        print(message)
-    messages = [{"role": "user", "content": message}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0
-    )
-    response_message = response["choices"][0]["message"]["content"]
-    return response_message 
-
-def query_message(
-    query: str,
-    df: pd.DataFrame,
-    model: str,
-    token_budget: int
-) -> str:
-    """Return a message for GPT, with relevant source texts pulled from a dataframe."""
-    strings, relatednesses = strings_ranked_by_relatedness(query, df)
-    introduction = 'You are helpful AI assistant, If the answer cannot be found in your training data, Use the below articles to answer the subsequent question. "'
-    question = f"\n\nQuestion: {query}"
-    message = introduction
-    for string in strings:
-        next_article = f'\n\nWikipedia article section:\n"""\n{string}\n"""'
-        if (
-            num_tokens(message + next_article + question, model=model)
-            > token_budget
-        ):
-            break
+    def ask(self, query: str, csv_path: str = None, df_cache: pd.DataFrame = None, token_budget: int = 4096 - 500) -> str:
+        if csv_path is None:
+            csv_path = self.embeddings_path
+            
+        if df_cache is None:
+            self.df = pd.read_csv(csv_path)
+            self.df['embedding'] = self.df['embedding'].apply(ast.literal_eval)
         else:
-            message += next_article
-    return message + question
+            self.df = df_cache
 
-def run_function_calling(query:str):
-    # Step1: send the conversation and available functions to GPT
-    messages = [
-    
-        {"role": "user", "content": query}
-    ]
-    functions = [
+        message = self.query_message(query, self.df, token_budget=token_budget)
+        messages = [{"role": "user", "content": message}]
+        response = openai.ChatCompletion.create(
+            model=self.GPT_MODEL,
+            messages=messages,
+            temperature=0
+        )
+        response_message = response["choices"][0]["message"]["content"]
+        return response_message 
+
+    def query_message(self, query: str, df: pd.DataFrame, token_budget: int) -> str:
+        strings, relatednesses = self.strings_ranked_by_relatedness(query, df)
+        introduction = 'You are helpful AI assistant, If the answer cannot be found in your training data, Use the below articles to answer the subsequent question. "'
+        question = f"\n\nQuestion: {query}"
+        message = introduction
+        for string in strings:
+            next_article = f'\n\nWikipedia article section:\n"""\n{string}\n"""'
+            if (
+                self.num_tokens(message + next_article + question, model=self.GPT_MODEL)
+                > token_budget
+            ):
+                break
+            else:
+                message += next_article
+        return message + question
+
+    def run_function_calling(self, query:str):
+        messages = [{"role": "user", "content": query}]
+        functions = [
         {
             "name": "ask",
             "description": "Answer a query using GPT and a dataframe of relevant texts and embeddings",
@@ -180,58 +150,47 @@ def run_function_calling(query:str):
                 "required": ["location", "unit", "num_days"]
             }
         }   
-    ]
-    response = openai.ChatCompletion.create(
-        model=GPT_MODEL,
-        messages=messages,
-        functions=functions,
-        function_call="auto",  # auto is default, but we'll be explicit
-    )
-    response_message = response["choices"][0]["message"]
-
-    # Step 2: check if GPT wanted to call a function
-    if response_message.get("function_call"):
-        # Step 3: call the function
-        # Note: the JSON response may not always be valid; be sure to handle errors
-        available_functions = {
-            "ask": ask,
-            "get_current_weather": get_current_weather,
-            "get_n_weather_forecast": get_n_weather_forecast
-        }  # three functions in this example
-        function_name = response_message["function_call"]["name"]
-        fuction_to_call = available_functions[function_name]
-        function_args = json.loads(response_message["function_call"]["arguments"])
-        if function_name =="ask":
-           function_response = fuction_to_call(query=function_args.get("answer"))
-        elif  function_name =="get_current_weather":
-              function_response = fuction_to_call(
-                location=function_args.get("location"),
-                unit=function_args.get("unit")
-              )
-        elif  function_name =="get_n_weather_forecast":
-              function_response = fuction_to_call(
-                location=function_args.get("location"),
-                unit=function_args.get("unit"),
-                num_days=function_args.get("num_days"),
-              )
-
-        # Step 4: send the info on the function call and function response to GPT
-        function_call_message={
-            "role": "function",
-            "name": function_name,
-            "content": function_response,
-        } 
-        return function_call_message
-"""
-        messages.append(function_call_message)  # extend conversation with function response
-        second_response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-0613",
+        ]
+        
+        response = openai.ChatCompletion.create(
+            model=self.GPT_MODEL,
             messages=messages,
-        )  # get a new response from GPT where it can see the function response
-    else:
-        second_response=response
+            functions=functions,
+            function_call="auto",
+        )
+        response_message = response["choices"][0]["message"]
 
-    return second_response["choices"][0]["message"]["content"]
-"""
-user_query = "2022卡塔尔世界杯最佳射手是谁？"
-print(run_function_calling(user_query))
+        if response_message.get("function_call"):
+            available_functions = {
+                "ask": self.ask,
+                "get_current_weather": self.get_current_weather,
+                "get_n_weather_forecast": self.get_n_weather_forecast
+            }
+            function_name = response_message["function_call"]["name"]
+            function_to_call = available_functions[function_name]
+            function_args = json.loads(response_message["function_call"]["arguments"])
+            if function_name =="ask":
+               function_response = function_to_call(query=function_args.get("answer"))
+            elif  function_name =="get_current_weather":
+                  function_response = function_to_call(
+                    location=function_args.get("location"),
+                    unit=function_args.get("unit")
+                  )
+            elif  function_name =="get_n_weather_forecast":
+                  function_response = function_to_call(
+                    location=function_args.get("location"),
+                    unit=function_args.get("unit"),
+                    num_days=function_args.get("num_days"),
+                  )
+            function_call_message={
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            }
+            messages.append(function_call_message) 
+            return messages
+
+# Now you can use the class to call the function
+runner = FunctionRunner(openai.api_key,GPT_MODEL,"/Volumes/work/Project/AIGC/OpenAI/Function_Call/data/FIFA_World_Cup_2022.csv")
+result=runner.run_function_calling("What's the weather like in Boston over the next 3 days?")
+print(result)
